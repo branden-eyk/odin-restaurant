09ce1451c2ad7dd821f85e92b8b60473d750f400
branden-eyk <brandeneyk@gmail.com>
2023-04-05 18:07:51 -0400

    Refactor home.js to create some closures. Add additional homepage content

--- src/home.js
+++ src/home.js
@@ -2,7 +2,10 @@ import Logo from './Odin-Face.jpg';
 
 function displayHome (){
     const contentDiv = document.querySelector('.content');
-    
+    contentDiv.append(createHeader(), createContent());
+}
+
+function createHeader(){
     const header = document.createElement('div');
     header.classList.add('header');
     
@@ -16,7 +19,44 @@ function displayHome (){
     title.classList.add('header__title');
 
     header.append(logo, title);
-    contentDiv.append(header);
+    return header;
 }
+
+function createContent(){
+    const content = document.createElement('div');
+    content.classList.add('content');
+
+    const aboutUs = document.createElement('p');
+    aboutUs.classList.add('content__subsection');
+    aboutUs.innerText = "Dare you, mortal, eat of the nectar of the gods? Dost thou possess the hubris that thou thinkest thee worthy to eat of the food that sustaineth thy masters? Then tarry not and cometh thee to Odin's Eatery of The Gods. Where the game ceases never to be fresh, and the mead dost eternally overfloweth."; 
+
+    const hours = document.createElement('div');
+    hours.classList.add('content__subsection');
+    const hoursSubtitle = document.createElement('h2');
+    hoursSubtitle.classList.add('content__subtitle');
+    hoursSubtitle.innerText = 'Hours';
+    const hoursList = document.createElement('ul');
+    hoursList.classList.add('content__list');
+    const hoursArray = ['Sunnandæg: 8am - 8pm', 'Monandæg: 9am - 9pm', 'Tiwesdæg: 9am - 9pm', 'Wodnesdæg: 6am - 11pm', 'Ðunresdæg: 8am - 10pm', 'Frigedæg: 8am - 10pm', 'Sæternesdæg: 8am - 10pm'];
+    for (const day of hoursArray) {
+        const item = document.createElement('li');
+        item.innerText = day;
+        hoursList.appendChild(item);
+    }
+    hours.append(hoursSubtitle, hoursList);
+
+    const location = document.createElement('div');
+    location.classList.add('content__subsection');
+    const locationSubtitle = document.createElement('h2');
+    locationSubtitle.classList.add('content__subtitle');
+    locationSubtitle.innerText = 'Location';
+    const locationText = document.createElement('p');
+    locationText.innerText = '246 Gungnir Ave., Valhalla, Asgard';
+    location.append(locationSubtitle, locationText);
+    
+    content.append(aboutUs, hours, location);
+    return content;
+}
+
 
 export default displayHome;
