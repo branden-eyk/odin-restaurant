5077666e528a6812a5eb54b05ec52584ffbda78a
branden-eyk <brandeneyk@gmail.com>
2023-04-12 15:53:33 -0400

    Add webpack dev server and add CSS styling for page content

--- src/home.js
+++ src/home.js
@@ -8,7 +8,7 @@ function displayHome (){
 
 function createHeader(){
     const header = document.createElement('div');
-    header.classList.add('header');
+    header.classList.add('header', 'content__div');
     
     const logo = document.createElement('img');
     logo.src = Logo;
@@ -25,19 +25,21 @@ function createHeader(){
 
 function createContent(){
     const content = document.createElement('div');
-    content.classList.add('content');
+    content.classList.add('pageContent');
 
-    const aboutUs = document.createElement('p');
-    aboutUs.classList.add('content__subsection');
-    aboutUs.innerText = "Dare you, mortal, eat of the nectar of the gods? Dost thou possess the hubris that thou thinkest thee worthy to eat of the food that sustaineth thy masters? Then tarry not and cometh thee to Odin's Eatery of The Gods. Where the game ceases never to be fresh, and the mead dost eternally overfloweth."; 
+    const aboutUs = document.createElement('div');
+    aboutUs.classList.add('content__div', 'pageContent__subsection');
+    const blurb = document.createElement('p');
+    blurb.innerText = "Dare you, mortal, eat of the nectar of the gods? Dost thou possess the hubris that thou thinkest thee worthy to eat of the food that sustaineth thy masters? Then tarry not and cometh thee to Odin's Eatery of The Gods. Where the game ceases never to be fresh, and the mead dost eternally overfloweth.";
+    aboutUs.appendChild(blurb); 
 
     const hours = document.createElement('div');
-    hours.classList.add('content__subsection');
+    hours.classList.add('pageContent__subsection', 'content__div');
     const hoursSubtitle = document.createElement('h2');
-    hoursSubtitle.classList.add('content__subtitle');
+    hoursSubtitle.classList.add('pageContent__subtitle');
     hoursSubtitle.innerText = 'Hours';
     const hoursList = document.createElement('ul');
-    hoursList.classList.add('content__list');
+    hoursList.classList.add('pageContent__list');
     const hoursArray = ['Sunnandæg: 8am - 8pm', 'Monandæg: 9am - 9pm', 'Tiwesdæg: 9am - 9pm', 'Wodnesdæg: 6am - 11pm', 'Ðunresdæg: 8am - 10pm', 'Frigedæg: 8am - 10pm', 'Sæternesdæg: 8am - 10pm'];
     for (const day of hoursArray) {
         const item = document.createElement('li');
@@ -47,9 +49,9 @@ function createContent(){
     hours.append(hoursSubtitle, hoursList);
 
     const location = document.createElement('div');
-    location.classList.add('content__subsection');
+    location.classList.add('pageContent__subsection', 'content__div');
     const locationSubtitle = document.createElement('h2');
-    locationSubtitle.classList.add('content__subtitle');
+    locationSubtitle.classList.add('pageContent__subtitle');
     locationSubtitle.innerText = 'Location';
     const locationText = document.createElement('p');
     locationText.innerText = '246 Gungnir Ave., Valhalla, Asgard';
